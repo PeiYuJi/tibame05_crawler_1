44e7c62c258d4cd6b059e58d46d5f55cdbe3a772
pei yu ji <abc0970870@gmail.com>
2025-09-01 22:56:15 +0800

    commit

--- build/lib/crawler/producer_main_us.py
+++
@@ -1,8 +0,0 @@
-from crawler.tasks_etf_list_us import etf_list_us  
-
-crawler_url = "https://tw.tradingview.com/markets/etfs/funds-usa/"
-
-# ETF 清單
-etf_list_us = etf_list_us.s(crawler_url="https://tw.tradingview.com/markets/etfs/funds-usa/")
-etf_list_us.apply_async(queue="etfus") 
-
